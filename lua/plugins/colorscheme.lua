2b386420b288549ed07db333077e9bf2998e21b8
Olivier Pelletier <olivier.op@outlook.com>
2024-11-30 14:04:13 -0500

    Config update.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,4 +5,14 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      }
+    }
+  }
 }
