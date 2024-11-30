4a44dd37bcf681765a36d207463889b0f4796bd6
Olivier Pelletier <olivier.op@outlook.com>
2024-11-30 14:19:46 -0500

    Config update.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -99,6 +99,7 @@ return {
   },
   {
     "tiagovla/scope.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
       require("scope").setup({})
       require("telescope").load_extension("scope")
