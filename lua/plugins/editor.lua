a151eeaf1ea47e7d3d87ea064c077b3e88424390
Olivier Pelletier <olivier.op@outlook.com>
2025-03-01 23:07:59 -0500

    Config update.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,17 +1,33 @@
 return {
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   opts = {
+  --     filesystem = {
+  --       filtered_items = {
+  --         hide_dotfiles = false,
+  --         hide_gitignored = false,
+  --       },
+  --       follow_current_files = {
+  --         enabled = true,
+  --         leave_dirs_open = true,
+  --       },
+  --       group_empty_dirs = true,
+  --     },
+  --   },
+  -- },
   {
-    "nvim-neo-tree/neo-tree.nvim",
+    "folke/snacks.nvim",
     opts = {
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+          },
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
         },
-        follow_current_files = {
-          enabled = true,
-          leave_dirs_open = true,
-        },
-        group_empty_dirs = true,
       },
     },
   },
