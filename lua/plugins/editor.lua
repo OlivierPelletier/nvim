79ec4e4afc78f6f53cf994f6a501379e10d09697
Olivier Pelletier <olivier.op@outlook.com>
2025-03-16 15:27:24 -0400

    Config update.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -97,35 +97,36 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local harpoon = require("harpoon")
+      ---@diagnostic disable-next-line: missing-parameter
       harpoon.setup()
 
-      -- vim.keymap.set("n", "<leader>a", function()
-      --   harpoon:list():add()
-      -- end)
-      vim.keymap.set("n", "<C-z>", function()
+      vim.keymap.set("n", "<M-q>", function()
+        harpoon:list():add()
+      end, { desc = "Harpoon Add" })
+      vim.keymap.set("n", "<M-/>", function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
+      end, { desc = "Harpoon Quick Menu" })
 
-    --   vim.keymap.set("n", "<C-h>", function()
-    --     harpoon:list():select(1)
-    --   end)
-    --   vim.keymap.set("n", "<C-t>", function()
-    --     harpoon:list():select(2)
-    --   end)
-    --   vim.keymap.set("n", "<C-n>", function()
-    --     harpoon:list():select(3)
-    --   end)
-    --   vim.keymap.set("n", "<C-s>", function()
-    --     harpoon:list():select(4)
-    --   end)
-    --
-    --   -- Toggle previous & next buffers stored within Harpoon list
-    --   vim.keymap.set("n", "<C-S-P>", function()
-    --     harpoon:list():prev()
-    --   end)
-    --   vim.keymap.set("n", "<C-S-N>", function()
-    --     harpoon:list():next()
-    --   end)
+      vim.keymap.set("n", "<M-1>", function()
+        harpoon:list():select(1)
+      end, { desc = "Harpoon Select 1" })
+      vim.keymap.set("n", "<M-2>", function()
+        harpoon:list():select(2)
+      end, { desc = "Harpoon Select 2" })
+      vim.keymap.set("n", "<M-3>", function()
+        harpoon:list():select(3)
+      end, { desc = "Harpoon Select 3" })
+      vim.keymap.set("n", "<M-4>", function()
+        harpoon:list():select(4)
+      end, { desc = "Harpoon Select 4" })
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<M-w>", function()
+        harpoon:list():prev()
+      end, { desc = "Harpoon Previous" })
+      vim.keymap.set("n", "<M-e>", function()
+        harpoon:list():next()
+      end, { desc = "Harpoon Next" })
     end,
   },
 }
