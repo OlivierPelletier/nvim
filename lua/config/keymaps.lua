e747a8ce4e91ae14f2154538c75632fd31ece295
Olivier Pelletier <olivier.op@outlook.com>
2025-04-26 00:40:49 -0400

    Config update.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.cmd("imap <C-c> <Esc>")
+
 vim.keymap.del("n", "<leader><tab><tab>")
 vim.keymap.del("n", "<leader><tab>]")
 vim.keymap.del("n", "<leader><tab>[")
