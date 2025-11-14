9f3cb5d181003482ed5851f856304bd5682bea53
Olivier Pelletier <olivier.op@outlook.com>
2025-11-14 08:56:19 -0500

    sync

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -2,8 +2,29 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
-      enabled = true,
+      enabled = false,
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
       win_options = { conceallevel = { rendered = 1 } },
     },
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = require("render-markdown").get,
+        set = require("render-markdown").set,
+      }):map("<leader>um")
+    end,
   },
 }
