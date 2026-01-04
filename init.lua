5b24f3e86510f829bd7478e15967e9805925a4f7
Olivier Pelletier <olivier.op@outlook.com>
2026-01-04 11:26:00 -0500

    sync

--- init.lua
+++ init.lua
@@ -20,14 +20,41 @@ vim.pack.add({
         { src = "https://github.com/folke/which-key.nvim" },
         { src = "https://github.com/neovim/nvim-lspconfig" },
         { src = "https://github.com/mason-org/mason.nvim" },
+        { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+        { src = "https://github.com/nvim-lua/plenary.nvim" },
+        { src = "https://github.com/ThePrimeagen/harpoon",           version = "harpoon2" },
+        { src = "https://github.com/folke/snacks.nvim" },
 })
 
 require("which-key").setup({
         preset = "helix"
 })
 require("mason").setup()
+require("nvim-treesitter").setup({
+        ensure_installed = { "lua", "go", "go", "gomod", "gowork", "gosum", "rust" },
+        highlight = { enable = true }
+})
+require("snacks").setup({
+        bigfile = { enabled = true },
+        dashboard = { enabled = true },
+        explorer = { enabled = true },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = { enabled = true },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+})
 
-vim.lsp.enable({ "lua_ls", "gopls", "rust_analyzer" })
+vim.api.nvim_create_autocmd('FileType', {
+        pattern = { "lua", "go", "rust" },
+        callback = function() vim.treesitter.start() end,
+})
+
+vim.lsp.enable({ "lua_ls", "gopls", "rust-analyzer" })
 vim.lsp.inlay_hint.enable(true)
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -43,5 +70,36 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end,
 })
 vim.cmd("set completeopt+=noselect")
+local harpoon = require("harpoon")
+---@diagnostic disable-next-line: missing-parameter
+harpoon.setup()
+
+vim.keymap.set("n", "<M-q>", function()
+        harpoon:list():add()
+end, { desc = "Harpoon Add" })
+vim.keymap.set("n", "<M-/>", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Harpoon Quick Menu" })
+
+vim.keymap.set("n", "<M-1>", function()
+        harpoon:list():select(1)
+end, { desc = "Harpoon Select 1" })
+vim.keymap.set("n", "<M-2>", function()
+        harpoon:list():select(2)
+end, { desc = "Harpoon Select 2" })
+vim.keymap.set("n", "<M-3>", function()
+        harpoon:list():select(3)
+end, { desc = "Harpoon Select 3" })
+vim.keymap.set("n", "<M-4>", function()
+        harpoon:list():select(4)
+end, { desc = "Harpoon Select 4" })
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<M-w>", function()
+        harpoon:list():prev()
+end, { desc = "Harpoon Previous" })
+vim.keymap.set("n", "<M-e>", function()
+        harpoon:list():next()
+end, { desc = "Harpoon Next" })
 
 vim.cmd("colorscheme catppuccin-frappe")
