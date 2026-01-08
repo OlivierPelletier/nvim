87e9a2953e7b51716e8f14f5b992bf7a2896e44c
Olivier Pelletier <olivier.op@outlook.com>
2026-01-07 23:42:34 -0500

    sync

--- lua/lang/vue.lua
+++ lua/lang/vue.lua
@@ -74,8 +74,25 @@ vim.lsp.config("vtsls", {
 vim.lsp.config("vue_ls", {})
 vim.lsp.enable({ "vtsls", "vue_ls" })
 
-Snacks.util.lsp.on({ name = "vtsls" }, function(_, client)
-	client.commands["_typescript.moveToFileRefactoring"] = function(command, _)
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client and client.name == "vtsls" then
+      -- stylua: ignore start
+      vim.keymap.set("n",  "gD", function() local win = vim.api.nvim_get_current_win() local params = vim.lsp.util.make_position_params(win, "utf-16") vim.lsp.buf_request(0, "workspace/executeCommand", { command = "typescript.goToSourceDefinition", arguments = { params.textDocument.uri, params.position }, open = true, }) end, {desc = "Goto Source Definition" })
+      vim.keymap.set("n",  "gR", function() vim.lsp.buf_request(0, "workspace/executeCommand", { command = "typescript.findAllFileReferences", arguments = { vim.uri_from_bufnr(0) }, open = true, }) end, {desc = "File References" })
+      vim.keymap.set("n",  "<leader>co", function() vim.lsp.buf.code_action({ apply = true, context = { only = { "source.organizeImports" }, diagnostics = {}}}) end, { desc = "Organize Imports" })
+      vim.keymap.set("n",  "<leader>cM", function() vim.lsp.buf.code_action({ apply = true, context = { only = { "source.addMissingImports.ts" }, diagnostics = {}}}) end, { desc = "Add missing imports" })
+      vim.keymap.set("n",  "<leader>cu", function() vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnused.ts" }, diagnostics = {}}}) end, { desc = "Remove unused imports" })
+      vim.keymap.set("n",  "<leader>cD", function() vim.lsp.buf.code_action({ apply = true, context = { only = { "source.fixAll.ts" }, diagnostics = {}}}) end, { desc = "Fix all diagnostics" })
+      vim.keymap.set("n",  "<leader>cV", function() vim.lsp.buf_request(0, "workspace/executeCommand", { command = "typescript.selectTypeScriptVersion" }) end, { desc = "Select TS workspace version" })
+			-- stylua: ignore end
+		end
+	end,
+})
+
+Snacks.util.lsp.on({ name = "vtsls" }, function(buffer, client)
+	client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
 		---@type string, string, lsp.Range
 		local action, uri, range = unpack(command.arguments)
 
