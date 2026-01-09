61b85e80e94e4d62d3728df03ddeda2b57418bf8
Olivier Pelletier <olivier.op@outlook.com>
2026-01-09 12:36:18 -0500

    sync

--- init.lua
+++ init.lua
@@ -97,10 +97,10 @@ vim.keymap.set("n", "<M-q>", function()	Harpoon:list():add()end, { desc = "Harpo
 vim.keymap.set("n", "<leader>,", Snacks.picker.buffers, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>/", function()	Snacks.picker.grep({ hidden = true })end, { desc = "Grep" })
 vim.keymap.set("n", "<leader><space>", function()	Snacks.picker.smart({ hidden = true })end, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>N", function() Snacks.picker.diagnostics() end, { desc = "Diagnostics" })
 vim.keymap.set("n", "<leader>bb", "<cmd>b#<CR>", { desc = "Previous Buffer" })
 vim.keymap.set("n", "<leader>bd", function()	Snacks.bufdelete()end, { desc = "Delete Buffer" })
 vim.keymap.set("n", "<leader>bo", Snacks.bufdelete.other, { desc = "Delete Other Buffers" })
-vim.keymap.set("n", "<leader>d", function() Snacks.picker.diagnostics() end, { desc = "Notifications" })
 vim.keymap.set("n", "<leader>fM", function()	MiniFiles.open(vim.uv.cwd())end, { desc = "Files Explorer (cwd)" })
 vim.keymap.set("n", "<leader>fm", function()	MiniFiles.open(vim.api.nvim_buf_get_name(0))end, { desc = "Files Explorer" })
 vim.keymap.set("n", "<leader>gd", function()	Snacks.picker.git_diff()end, { desc = "Git Diff (Hunks)" })
@@ -272,15 +272,16 @@ vim.api.nvim_create_autocmd("UIEnter", {
 		vim.print = _G.dd
 
     -- stylua: ignore start
-		Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-		Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
 		Snacks.toggle.diagnostics():map("<leader>ud")
+		Snacks.toggle.dim():map("<leader>uD")
+		Snacks.toggle.indent():map("<leader>ug")
+		Snacks.toggle.inlay_hints():map("<leader>uh")
 		Snacks.toggle.line_number():map("<leader>ul")
 		Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+		Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+		Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+		Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
 		Snacks.toggle.treesitter():map("<leader>uT")		Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-		Snacks.toggle.inlay_hints():map("<leader>uh")
-		Snacks.toggle.indent():map("<leader>ug")
-		Snacks.toggle.dim():map("<leader>uD")
 		-- stylua: ignore end
 	end,
 })
