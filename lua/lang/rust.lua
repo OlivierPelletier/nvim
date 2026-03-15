d536067cddbe2248a30c2071ff9388a812a4c9a5
Olivier Pelletier <olivier.op@outlook.com>
2026-03-15 14:56:38 -0400

    sync

--- lua/lang/rust.lua
+++ lua/lang/rust.lua
@@ -1,10 +1,5 @@
 require("util")
 
-local languageServersAndTools = {
-	"rust-analyzer",
-}
-
-MasonCheckAndInstallPackages(languageServersAndTools)
-
-vim.lsp.config("rust_analyzer", {})
-vim.lsp.enable("rust_analyzer")
+vim.pack.add({
+	"https://github.com/mrcjkb/rustaceanvim",
+})
